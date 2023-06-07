c0d7d5776cbe9c180de496fef956eba33af730ba
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2023-06-07 17:42:53 +0300

    [add] zombie animation

--- Assets/4_Script/Enemy/SimpleEnemy/SimpleEnemy.cs
+++ Assets/4_Script/Enemy/SimpleEnemy/SimpleEnemy.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class SimpleEnemy : Enemy
 {
+    [Header("Components")]
+    [SerializeField] protected Animator animator;
 
+    public override void EveryFrame()
+    {
+        base.EveryFrame();
+
+        animator.SetFloat("distance", distanceToTarget);
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+
+        animator.SetTrigger("attack");
+    }
 }
