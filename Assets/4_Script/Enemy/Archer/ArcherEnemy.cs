2b7c9e5c210b63e78d6e092362f3660504b335f2
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2023-06-11 13:35:41 +0300

    [add] archer walk animation

--- Assets/4_Script/Enemy/Archer/ArcherEnemy.cs
+++ Assets/4_Script/Enemy/Archer/ArcherEnemy.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class ArcherEnemy : Enemy
 {
+    [Header("Components")]
+    [SerializeField] protected Animator animator;
+    public override void EveryFrame()
+    {
+        base.EveryFrame();
+
+        animator.SetFloat("distance", distanceToTarget);
+    }
     protected override void Attack()
     {
-        Debug.Log("Archer");
         lastedTimeFromLastAttack = 0;
     }
 }
